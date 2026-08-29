092b04a0f56c708457688cd9c4e695c6e1499218
5Maurice5 <maldunated@fcpn.edu.bo>
2026-08-29 18:57:02 -0400

    TLB-6: Importar getAll en App.jsx y mostrar respuesta en consola

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,23 @@
-const App = () => {
+import { useEffect } from "react"
+import { getAllTask } from "./services/tarea.service"
+
+function App() {
+
+  useEffect(() => {
+    const loadTasks = async () => {
+      try {
+        const tasks = await getAllTask();
+        console.log(tasks);
+        
+      } catch (error) {
+        console.error("Error al obtener las tareas: ", error);
+      }
+    }
+    loadTasks();
+  }, [])
+
   return (
-    <><h1>Hola mundo</h1></>
+    <div>Ticket 6 - Consumo de API</div>
   )
 }
 export default App
--- src/services/tarea.service.js
+++ src/services/tarea.service.js
@@ -1,11 +1,12 @@
-const API_URL = import.meta.env.API;
+const API_URL = import.meta.env.VITE_API_URL;
 
 export const getAllTask = async () => {
-  const response = await fetch(`${API_URL}/tareas`);
+  const response = await fetch(`${API_URL}/tasks`);
 
   if (!response.ok) {
     throw new Error("Error al obtener las tareas");
   }
 
-  return await response.json();
+  const result = await response.json();
+  return result.data;
 };
