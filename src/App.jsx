5c700e519b5cc185c862c7157d8754ff2f5c98e6
5Maurice5 <maldunated@fcpn.edu.bo>
2026-08-30 17:37:00 -0400

    TLB-17: Login

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
 import DashboardLayout from "./components/layout/DashboardLayout";
 
+import Login from "./components/auth/Login";
+
 import TaskList from "./components/tasks/TaskList";
 import UserList from "./components/users/UserList";
 import CategoryList from "./components/categories/CategoryList";
@@ -11,12 +13,21 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+        {/* LOGIN */}
+
+        <Route path="/login" element={<Login />} />
+
+        {/* DASHBOARD */}
+
         <Route path="/" element={<DashboardLayout />}>
           <Route index element={<Navigate to="/tasks" replace />} />
 
           <Route path="tasks" element={<TaskList />} />
+
           <Route path="users" element={<UserList />} />
+
           <Route path="categories" element={<CategoryList />} />
+
           <Route path="tags" element={<TagList />} />
         </Route>
       </Routes>
