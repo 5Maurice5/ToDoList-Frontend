c8c3a4a9f2c4ce743a36b0a7f180aa2407934249
5Maurice5 <maldunated@fcpn.edu.bo>
2026-08-30 16:53:19 -0400

    TLB-13: Ticket 13 – Crear tarea (con categorías y tags)

--- src/services/tarea.service.js
+++ src/services/tarea.service.js
@@ -1,6 +1,6 @@
 const API_URL = import.meta.env.VITE_API_URL;
 
-export const getAllTask = async () => {
+export const getAll = async () => {
   const response = await fetch(`${API_URL}/tasks`);
 
   if (!response.ok) {
@@ -8,5 +8,68 @@ export const getAllTask = async () => {
   }
 
   const result = await response.json();
+
+  return result.data;
+};
+
+export const create = async (task) => {
+  const response = await fetch(`${API_URL}/tasks`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(task),
+  });
+
+  if (!response.ok) {
+    throw new Error("Error al crear la tarea");
+  }
+
+  const result = await response.json();
+
+  return result.data;
+};
+
+export const update = async (id, task) => {
+  const response = await fetch(`${API_URL}/tasks/${id}`, {
+    method: "PUT",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(task),
+  });
+
+  if (!response.ok) {
+    throw new Error("Error al actualizar la tarea");
+  }
+
+  const result = await response.json();
+
+  return result.data;
+};
+
+export const deleteTask = async (id) => {
+  const response = await fetch(`${API_URL}/tasks/${id}`, {
+    method: "DELETE",
+  });
+
+  if (!response.ok) {
+    throw new Error("Error al eliminar la tarea");
+  }
+
+  const result = await response.json();
+
+  return result.data;
+};
+
+export const getOne = async (id) => {
+  const response = await fetch(`${API_URL}/tasks/${id}`);
+
+  if (!response.ok) {
+    throw new Error("Error al obtener la tarea");
+  }
+
+  const result = await response.json();
+
   return result.data;
 };
