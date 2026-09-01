bdb880d631af24d55b1bfc9af974eb37f0e89038
5Maurice5 <maldunated@fcpn.edu.bo>
2026-09-01 11:14:40 -0400

    TLB-6-Fix: Comentario de tareas quitado - useEffect mas limpio

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,18 @@
-import { useEffect } from "react"
-import { getAllTask } from "./services/tarea.service"
+import { useEffect } from "react";
+import { getAllTask } from "./services/tarea.service";
 
 function App() {
-
   useEffect(() => {
-    const loadTasks = async () => {
-      try {
-        const tasks = await getAllTask();
-        console.log(tasks);
-        
-      } catch (error) {
-        console.error("Error al obtener las tareas: ", error);
-      }
-    }
     loadTasks();
-  }, [])
+  }, []);
 
-  return (
-    <div>Ticket 6 - Consumo de API</div>
-  )
+  const loadTasks = async () => {
+    try {
+      const tasks = await getAllTask();
+    } catch (error) {
+      console.error("Error al obtener las tareas: ", error);
+    }
+  };
+  return <div>Ticket 6 - Consumo de API</div>;
 }
-export default App
+export default App;
